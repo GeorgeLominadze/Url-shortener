5cc9f3f32bf58f27165e4a69baf96a1d41d316d4
Gio Elv <giolom548@gmail.com>
2023-05-04 13:59:55 +0400

    first commit

---
+++ shortner/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+import uuid
+from .models import Url
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def create(request):
+    if request.method == "POST":
+        link = request.POST['link']
+        uid = str(uuid.uuid4())[:5]
+        new_url = Url(link=link, uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
+
+
+def go(request, pk):
+    url_details = Url.objects.get(uuid=pk)
+    link = url_details.link
+    if not link.startswith('https://'):
+        if link.startswith('www.'):
+            link = 'https://'+link
+        else:
+            link = 'https://www.'+link
+    return redirect(link)
